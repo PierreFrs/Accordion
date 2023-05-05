8a3258e3a508ef068f72c53eb06e7fc171f1c908
Pierre <pierrefraisse90@gmail.com>
2023-05-05 15:18:25 +0200

    Logic

--- src/components/faq/Faq.js
+++ src/components/faq/Faq.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Question from './Question';
+import {questions} from './data'
 import { RiQuestionnaireFill } from 'react-icons/ri';
 
 const Faq = () => {
@@ -12,9 +13,10 @@ const Faq = () => {
                 <p className='--text-small'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sit veritatis ea eveniet tenetur porro culpa labore voluptates, nisi aliquam rem. Eius praesentium est dolor perspiciatis temporibus ex, quo eligendi atque?</p>
             </div>
             <div className="questions --center-all">
-                <Question />
-                <Question />
-                <Question />
+                {questions.map((question) => (
+                    <Question key={question.id} title={question.title} answer={question.answer}/>
+                ))}
+                
             </div>
         </div>
         
